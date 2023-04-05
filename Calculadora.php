4402295c89b3706ce782f1c1816192b8abfc4340
Luis Santos <117953034+LuisGabrielMascarenhas@users.noreply.github.com>
2023-04-05 20:19:35 -0300

    Criação da function exibir e erro

--- Calculadora.php
+++ Calculadora.php
@@ -1,63 +1,66 @@
 <?php 
 
 $num1 = 10;
-$num2 = 0;
-echo "Calculadora<br/>";
-echo "Insira dois números<br/>";
-$x = "+";
+$num2 = 5;
+$x = "/";
 $res = 0;
-echo "Números Inseridos foram ".$num1." e ".$num2."<br/>";
-echo "Escolha o operador<br/>+<br/>-<br/>x<br/>/<br/>";
+echo "Calculadora<br/>";
+echo "Insira o primeiro número<br/>".$num1;
+echo "<br/><br/>Escolha o operador<br/>+ - x /<br/>";
 echo "O operador escolhido foi ".$x."<br/>";
+echo "<br/>Insira o segundo número<br/>".$num2;
+echo "<br/><br/>Números Inseridos foram ".$num1.$x.$num2."<br/>";
 
-
-function soma($num1,$num2){
-	
+function soma($num1,$num2){	
 	$res = $num1 + $num2;
-	echo "O Resultado da soma é:".$res;
 	return$res;
 
 }
-function sub($num1,$num2){
-	
+
+function sub($num1,$num2){	
 	$res = $num1 - $num2;
-	echo "O Resultado da subtração é:".$res;
 	return$res;
 
 }
 function mult($num1,$num2){
-	
 	$res = $num1 * $num2;
-	echo "O Resultado da multiplicação é:".$res;
 	return$res;
 
 }
-function div($num1,$num2){
-	if ($num1 == 0 || $num2 == 0) {
-		echo "0 não é valido na divisão";
-	}else{
-	$res = $num1 / $num2;
-	echo "O Resultado da divisão é:".$res;
-	return$res;	
-	}
-	
 
+function div($num1,$num2){
+	$res = $num1 / $num2;
+	return$res;	
 }
+	
+function exibir($res){
+	echo "O Resultado é:" .$res;
+	return$res;
+}
+
+function erro(){
+	echo "Impossivel dividir por 0";
+}
+
 switch ($x) {
 	case '+':
-		soma($num1,$num2);
-		break;
+		exibir(soma($num1,$num2));
+		break;	
 	
 	case '-':
-		sub($num1,$num2);
+		exibir(sub($num1,$num2));
 		break;
 
 	case 'x':
-		mult($num1,$num2);
+		exibir(mult($num1,$num2));
 		break;
 
 	case '/':
-		div($num1,$num2);
+		if ($num2 == 0) {
+			erro();
+		}else{
+			exibir(div($num1,$num2));
+}
 		break;
 
 	default:
